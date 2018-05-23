6d5a27d5cfcde202a2f43264b1a13b6497ed4b82
QX <44708869@qq.com>
2018-05-23 18:14:10 +0800

    a

---
+++ hulib.h
@@ -0,0 +1,11 @@
+//
+// Created by ok on 2018/5/23.
+//
+
+#ifndef UNTITLED_HULIB_H
+#define UNTITLED_HULIB_H
+#pragma once
+
+int get_huxi(char* cards);
+
+#endif //UNTITLED_HULIB_H
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "hulib.h"
+
+void testhuxi() {
+    char cards[] = {0, 1, 2, 1, 2, 2, 0, 0, 1, 0, 0, 1, 3, 2, 2, 2, 0, 0, 2, 0};
+    printf("胡息: %d\n", get_huxi(cards));
+}
+int main() {
+    printf("Hello, World!\n");
+    testhuxi();
+    return 0;
+}
