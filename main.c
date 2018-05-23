df902e19f9ee25f1aa38ff8d732200a1a3d3fb46
QX <44708869@qq.com>
2018-05-23 18:34:44 +0800

    Wed May 23 18:34:44 CST 2018

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include "hulib.h"
 
 void testhuxi() {
-    char cards[] = {0, 1, 2, 1, 2, 2, 0, 0, 1, 0, 0, 1, 3, 2, 2, 2, 0, 0, 2, 0};
+//    char cards[] = {0, 1, 2, 1, 2, 2, 0, 0, 1, 0, 0, 1, 3, 2, 2, 2, 0, 0, 2, 0};
+    char cards[] = {0, 2, 1, 2, 2, 1, 0, 2, 1, 1, 0, 1, 0, 0, 1, 1, 2, 2, 0, 1};
     printf("胡息: %d\n", get_huxi(cards));
 }
 int main() {
